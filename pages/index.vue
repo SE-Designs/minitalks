5821caec9e3716d4efc3783ea9b993d48a4486b1
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-04 14:56:43 +0600

    add: index page

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col mt-[40px] w-full">
+    <div class="flex flex-row justify-between w-full gap-x-8 px-8">
+      <AppLeftAside />
+      <AppMainSection />
+      <AppRightAside />
+    </div>
+  </div>
+</template>
