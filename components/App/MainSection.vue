4296c4e705291b1c46db963564d1c1d3541d635f
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-04 14:57:24 +0600

    add: app main

---
+++ components/App/MainSection.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="flex flex-col gap-y-8 w-full">
+    <div class="flex flex-col p-4 bg-neutral rounded-lg"></div>
+  </section>
+</template>
