0502672c5da2d0993091248a79c305ceddfc940d
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-04 16:19:31 +0600

    add: clean layout

---
+++ layouts/clean.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <main
+    class="flex flex-col justify-center items-center max-w-[1480px] w-full mx-auto"
+  >
+    <slot />
+  </main>
+</template>
