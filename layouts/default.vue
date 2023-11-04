4c547a93be10930dd9badda4cb7d58082263f88a
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-04 14:56:35 +0600

    add: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <LayoutNav />
+  </header>
+  <main
+    class="mt-[60px] flex flex-col justify-center items-center max-w-[1480px] w-full mx-auto"
+  >
+    <slot />
+  </main>
+</template>
