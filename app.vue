bc6dc0442cc5a791c0327ebcd452814202680260
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-06 21:34:44 +0600

    style: modal

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<style>
+html {
+  scroll-behavior: smooth;
+  min-height: 100vh;
+}
+
+dialog {
+  transition: backdrop-filter 0.2s;
+  backdrop-filter: blur(8px);
+}
+
+.modal-box {
+  border: 1px solid rgba(222, 222, 222, 0.2);
+  background: rgba(0, 0, 0, 0.4);
+}
+</style>
