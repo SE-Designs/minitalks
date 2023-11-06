a7b3d3c1aa4b94fd3914554fb254f6549fd073f6
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-06 21:35:21 +0600

    style: index, main

--- components/App/MainSection.vue
+++ components/App/MainSection.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts"></script>
 <template>
   <section class="flex flex-col gap-y-8 flex-1">
-    <!-- <AppWrite /> -->
-    <div class="flex flex-col gap-y-4 bg-neutral rounded-lg p-6 xl:p-8">
-      <AppFilter />
+    <AppFilter />
+    <AppWrite />
+
+    <div
+      class="flex flex-col gap-y-12 bg-neutral rounded-lg p-6 xl:p-8 mb-28 sm:mb-0"
+    >
+      <AppPost />
       <div class="w-full h-[3px] bg-base-100 rounded" />
       <AppPost />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-col mt-12 w-full">
+  <div class="flex flex-col gap-8 mt-12 w-full px-4">
+    <AppTopAside />
     <div
-      class="flex flex-row justify-between w-full gap-x-4 px-4 xl:gap-x-8 xl:px-8"
+      class="flex flex-row justify-between w-full gap-x-4 xl:gap-x-8 xl:px-8"
     >
       <AppLeftAside />
       <AppMainSection />
