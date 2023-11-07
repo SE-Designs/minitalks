6ae18aeb7bfa82f61a6d857d8627000b678f8b15
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-07 17:42:35 +0600

    deps: bcrypt -> bcryptjs; upd: runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  runtimeConfig: {
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+  },
 });
