178594d9ba43d096b361cbd0c0e414170a593c25
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-08 21:19:21 +0600

    add: auth middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,8 @@
+// export default defineNuxtRouteMiddleware(async (to, from, next) => {
+//   if (process.server) return;
+//   const { useAuthUser, initAuth } = useAuth();
+
+//   initAuth()
+
+//   // if (!user.value) return navigateTo("/auth#sign-in");
+// });
