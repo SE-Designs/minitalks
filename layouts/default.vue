f5a5672c7381a26d0dedcfb4c6b1377c6eccb477
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 16:41:07 +0600

    upd: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,37 @@
 <script setup lang="ts">
 import { isWriteModalOpen } from "#imports";
+import type { ShortUserType } from "~/types/types";
 
-const { useAuthLoading, useAuthUser } = useAuth();
-const user = useAuthUser();
+const { useAuthLoading, useAuthUser } = await useAuth();
+const user = (useAuthUser() || "") as unknown as ShortUserType;
 const isLoading = useAuthLoading();
 </script>
 <template>
-  <header>
-    <LayoutNav />
-  </header>
-  <main
-    class="mt-[60px] flex flex-col justify-center items-center max-w-[1480px] w-full mx-auto"
-    v-if="!isLoading"
-  >
-    <div class="my-12 w-full">
-      <slot />
-      {{ user }}
-    </div>
-    <dialog
-      id="write"
-      class="flex justify-center items-center"
-      :class="isWriteModalOpen ? 'modal modal-open' : 'modal'"
-      @click.self="useWriteModal"
-    >
-      <AppWrite />
-      <form method="dialog" class="modal-backdrop">
-        <button>close</button>
-      </form>
-    </dialog>
-  </main>
-  <main class="w-screen h-screen flex justify-center items-center">
-    <span class="loading loading-spinner"></span>
-  </main>
+  <ClientOnly>
+    <main v-if="!isLoading">
+      <header>
+        <LayoutNav />
+      </header>
+      <section
+        class="mt-[60px] flex flex-col justify-center items-center max-w-[1480px] w-full mx-auto"
+      >
+        <div class="my-12 w-full">
+          <slot />
+          {{ user }}
+        </div>
+        <dialog
+          id="write"
+          class="flex justify-center items-center"
+          :class="isWriteModalOpen ? 'modal modal-open' : 'modal'"
+          @click.self="useWriteModal"
+        >
+          <AppWrite />
+          <form method="dialog" class="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </section>
+    </main>
+    <LayoutLoading v-else />
+  </ClientOnly>
 </template>
