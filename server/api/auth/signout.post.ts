659cbccc793546df0504e78b9835364ce375f2a0
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 22:27:18 +0600

    add: signout post

---
+++ server/api/auth/signout.post.ts
@@ -0,0 +1,24 @@
+import { removeRefreshToken } from "~/server/db/refreshToken";
+import sendRefreshToken from "~/utils/sendRefreshToken";
+
+export default defineEventHandler(async (event) => {
+  try {
+    const cookies = parseCookies(event);
+
+    const refreshToken = cookies.refresh_token;
+
+    await removeRefreshToken(refreshToken);
+  } catch (error) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 500,
+        statusMessage: `Something went wrong: ${error}`,
+      })
+    );
+  }
+
+  sendRefreshToken(event.res, "");
+
+  return { message: "Done" };
+});
