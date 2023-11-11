a0e428bf734c19c057fc5914e0a2c8f2f4a1533e
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 16:36:32 +0600

    add: types

---
+++ types/types.ts
@@ -0,0 +1,47 @@
+export type DecodedTokenType = {
+  userId: string;
+  iat: number;
+  exp: number;
+};
+
+export type TokenType = {
+  id: string;
+  token: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+};
+
+export type MininoteType = {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: string;
+  author: UserType;
+  likedIds: string[];
+  viewedIds: string[];
+  replyToId?: string;
+  replyTo?: string;
+  replies: string[];
+};
+
+export type UserType = {
+  id: string;
+  email: string;
+  username: string;
+  password?: string;
+  avatar: string | null;
+  following: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  refreshToken: TokenType[];
+  mininote: MininoteType[];
+};
+
+export type PublicUserType = Pick<
+  UserType,
+  "id" | "email" | "username" | "avatar" | "following" | "mininote"
+>;
+
+export type ShortUserType = Pick<UserType, "id" | "email" | "username">;
