cc92e58f26cc99a0b7ab22d03d3de010c7edd9a3
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 22:32:25 +0600

    upd: main section to loop and send props to child

--- components/App/MainSection.vue
+++ components/App/MainSection.vue
@@ -1,17 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  posts: {
+    type: Array,
+    required: true,
+  } as any,
+});
+</script>
 <template>
   <section class="flex flex-col gap-y-8 flex-1">
-    <AppFilter />
-    <!-- <AppWrite /> -->
-
-    <div
-      class="flex flex-col gap-y-12 bg-neutral rounded-lg p-6 xl:p-8 mb-28 sm:mb-0"
-    >
-      <AppPost />
-      <div class="w-full h-[3px] bg-base-100 rounded" />
-      <AppPost />
-      <div class="w-full h-[3px] bg-base-100 rounded" />
-      <AppPost />
+    <div class="flex flex-col bg-neutral rounded-lg p-6 xl:p-8 mb-28 sm:mb-0">
+      <div
+        class="flex flex-col gap-y-12 w-full"
+        v-for="mininote in props.posts"
+        :key="mininote.id"
+      >
+        <AppPost :post="mininote" />
+        <div class="w-full h-[3px] bg-base-100 rounded" />
+      </div>
     </div>
   </section>
 </template>
