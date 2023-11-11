7f4c7098e0b36bb3c3b2c187e5e2e0b37abaeb41
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 22:26:49 +0600

    add: mininote transformer

--- server/transformers/mininote.ts
+++ server/transformers/mininote.ts
@@ -1,7 +1,23 @@
-function mininoteTransformer(mininote: any) {
+import human from "human-time";
+import { mediaFileTransformer } from "./mediafile";
+import { userTransformer } from "./user";
+
+function mininoteTransformer(mininote: any): any {
   return {
     id: mininote.id,
     content: mininote.content,
+    mediaFiles: mininote.mediaFiles
+      ? mininote.mediaFiles.map(mediaFileTransformer)
+      : [],
+    author: mininote.author ? userTransformer(mininote.author) : null,
+    replies: mininote.replies ? mininote.replies.map(mininoteTransformer) : [],
+    replyTo: mininote.replyTo ? mininoteTransformer(mininote.replyTo) : null,
+    repliesNumber: mininote.replies ? mininote.replies.length : 0,
+    time: human(mininote.createdAt),
+    likedIds: mininote.likedIds,
+    likedNumber: mininote.likedIds ? mininote.likedIds.length : 0,
+    viewedIds: mininote.viewedIds,
+    viewedNumber: mininote.viewedIds ? mininote.viewedIds.length : 0,
   };
 }
 
