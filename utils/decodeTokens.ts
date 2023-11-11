5e8f3e17b3e46c4b92b60d0042b2c547311a5c4f
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 16:39:27 +0600

    upd: decode tokens

--- utils/decodeTokens.ts
+++ utils/decodeTokens.ts
@@ -1,22 +1,34 @@
 import jwt from "jsonwebtoken";
 
-function decodeAccessToken(token: any) {
+/**
+ * Decode Access Token:
+ ** accept: token (string)
+ ** returns:
+ *
+ */
+function decodeAccessToken(token: string) {
   const config = useRuntimeConfig();
 
   try {
-    return jwt.verify(token, config.jwtAccessSecret) as any;
+    return jwt.verify(token, config.jwtAccessSecret);
   } catch (error) {
-    return null;
+    return console.error(`DECODE ACCESS ERR: ${error}`);
   }
 }
 
-function decodeRefreshToken(token: any) {
+/**
+ * Decode Refresh Token:
+ ** accept: token (string)
+ ** returns:
+ *
+ */
+function decodeRefreshToken(token: string) {
   const config = useRuntimeConfig();
 
   try {
-    return jwt.verify(token, config.jwtRefreshSecret) as any;
+    return jwt.verify(token, config.jwtRefreshSecret);
   } catch (error) {
-    return null;
+    return console.error(`DECODE REFRESH ERR: ${error}`);
   }
 }
 
