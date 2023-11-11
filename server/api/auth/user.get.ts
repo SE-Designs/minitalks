48f2e5c4a6b7d7d451661f870d4a77713aba6616
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 16:38:56 +0600

    upd: user get

--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,7 +1,9 @@
+import { ShortUserType } from "~/types/types";
 import { userTransformer } from "~/server/transformers/user";
 
 export default defineEventHandler(async (event) => {
+  console.log(event.context.auth.user);
   return {
-    user: userTransformer(event.context.auth?.user),
+    user: userTransformer(event.context.auth?.user) as ShortUserType,
   };
 });
