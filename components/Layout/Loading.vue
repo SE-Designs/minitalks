1a59e734df684b0129b10bfc991b26917ab01ecf
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 16:34:47 +0600

    add: loading component

---
+++ components/Layout/Loading.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="w-screen h-screen flex justify-center items-center">
+    <span class="loading loading-spinner"></span>
+  </section>
+</template>
