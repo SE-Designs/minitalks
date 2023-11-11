7d5afbac905175ac234375d6a26f1ee4e280eb75
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-11 19:43:00 +0600

    deps: cloudinary; upd: cloud config envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,8 @@ export default defineNuxtConfig({
     baseUrl: process.env.BASE_URL,
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    cloud_api_key: process.env.CLOUD_API_KEY,
+    cloud_api_secret: process.env.CLOUD_API_SECRET,
   },
 });
