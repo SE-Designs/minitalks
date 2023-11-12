fa9caa57028292592e023bfd1165b72559186f01
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-12 20:09:27 +0600

    upd: index (home), [id] (mininote) pages

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@ const user = (useAuthUser() || "") as unknown as ShortUserType;
 const homeMininotes = ref([]) as any;
 const loading = ref(false);
 
-onBeforeMount(async () => {
+async function getMininotes() {
   loading.value = true;
   try {
     const { mininotes }: any = await getHomeMininotes();
@@ -23,36 +23,21 @@ onBeforeMount(async () => {
   } finally {
     loading.value = false;
   }
+}
+
+onBeforeMount(async () => {
+  await getMininotes();
 });
 </script>
 <template>
-  <div class="flex flex-col gap-8 w-full px-4">
-    <div class="flex flex-col gap-2 text-center">
-      <h2 class="font-bold text-3xl">
-        its <span class="text-gradient">minitalks</span>
-      </h2>
-      <p class="text-xs">
-        How are you {{ user?.username }}?
-        <a href="#" class="link link-accent italic">start writing</a>
-      </p>
-    </div>
-
-    <AppTopAside />
-    <div
-      class="flex flex-row justify-between w-full gap-x-4 xl:gap-x-8 xl:px-8"
-    >
-      <AppLeftAside />
-      <div class="flex flex-col gap-y-8 flex-1">
-        <AppFilter />
-        <AppMainFallback v-if="loading" />
-        <AppMainSection
-          v-else-if="homeMininotes.length > 0"
-          :posts="homeMininotes"
-        />
-        <AppMainNotFound v-else-if="homeMininotes.length === 0" />
-        <AppMainError v-else />
-      </div>
-      <AppRightAside />
-    </div>
+  <div class="flex flex-col gap-y-8 flex-1">
+    <AppFilter />
+    <AppMainFallback v-if="loading" />
+    <AppMainSection
+      v-else-if="homeMininotes.length > 0"
+      :posts="homeMininotes"
+    />
+    <AppMainNotFound v-else-if="homeMininotes.length === 0" />
+    <AppMainError v-else />
   </div>
 </template>
--- pages/mininote/[id].vue
+++ pages/mininote/[id].vue
@@ -30,28 +30,10 @@ onBeforeMount(async () => {
 });
 </script>
 <template>
-  <div class="flex flex-col gap-8 w-full px-4">
-    <div class="flex flex-col gap-2 text-center">
-      <h2 class="font-bold text-3xl">
-        its
-        <span class="text-gradient">minitalks</span>
-      </h2>
-      <p class="text-xs">
-        <a href="#" class="link link-accent italic">start writing</a>
-      </p>
-      <AppTopAside />
-      <div
-        class="flex flex-row justify-between w-full gap-x-4 xl:gap-x-8 xl:px-8"
-      >
-        <AppLeftAside />
-        <div class="flex flex-col gap-y-8 flex-1">
-          <AppMainFallback v-if="loading" />
-          <AppMainNotFound v-else-if="isPromise(mininote)" />
-          <AppMainSection v-else-if="mininote" :posts="mininote" />
-          <AppMainError v-else />
-        </div>
-        <AppRightAside />
-      </div>
-    </div>
+  <div class="flex flex-col gap-y-8 flex-1">
+    <AppMainFallback v-if="loading" />
+    <AppMainNotFound v-else-if="isPromise(mininote)" />
+    <AppMainSection v-else-if="mininote" :posts="mininote" />
+    <AppMainError v-else />
   </div>
 </template>
