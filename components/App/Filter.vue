d923a31df5562560cf62ef46764d2545d1eaec99
SE-Designs <dragonlorefnstattrack@gmail.com>
2023-11-14 20:42:12 +0600

    upd: filter - 4 options

--- components/App/Filter.vue
+++ components/App/Filter.vue
@@ -1,34 +1,46 @@
 <script setup lang="ts">
 const show = ref("latest");
+
+const emits = defineEmits(["setShow"]);
+
+const sortLinks = ref([
+  {
+    name: "latest",
+    icon: "pixelarticons:hourglass",
+  },
+  {
+    name: "best",
+    icon: "pixelarticons:trophy",
+  },
+  {
+    name: "popular",
+    icon: "pixelarticons:cellular-signal-3",
+  },
+  {
+    name: "interesting",
+    icon: "pixelarticons:ac",
+  },
+]);
+
+function setShow(category: string) {
+  show.value = category;
+
+  emits("setShow", show.value);
+}
 </script>
 <template>
   <div class="flex flex-col justify-between items-center sm:flex-row">
     <p class="text-secondary font-bold">showing {{ show }}:</p>
-    <div class="flex flex-row items-center gap-x-4 font-bold">
+    <div class="flex flex-row items-center gap-x-6 font-bold">
       <div
+        v-for="link in sortLinks"
+        :key="link.name"
         class="flex flex-row items-center gap-x-2 link link-hover hover:text-white"
-        :class="show === 'latest' ? 'link-success' : ''"
-        @click="show = 'latest'"
+        :class="show === link.name ? 'link-success' : ''"
+        @click="setShow(link.name)"
       >
-        <Icon name="pixelarticons:hourglass" />
-        <p>latest</p>
-      </div>
-      <div
-        class="flex flex-row items-center gap-x-2 link link-hover hover:text-white"
-        :class="show === 'trending' ? 'link-success' : ''"
-        @click="show = 'trending'"
-      >
-        <Icon name="pixelarticons:cellular-signal-2" />
-        <p>trending</p>
-      </div>
-
-      <div
-        class="flex flex-row items-center gap-x-2 link link-hover hover:text-white"
-        :class="show === 'best' ? 'link-success' : ''"
-        @click="show = 'best'"
-      >
-        <Icon name="pixelarticons:trophy" />
-        <p>best</p>
+        <Icon :name="link.icon" />
+        <p>{{ link.name }}</p>
       </div>
     </div>
   </div>
